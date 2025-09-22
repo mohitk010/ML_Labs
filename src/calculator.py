5e9a20a4245114665c1360a6a133de101c6706e2
mohitk010 <mohitkakda08@gmail.com>
2025-09-22 02:59:16 +0100

    Second Try unit and py tests + workflow files

--- src/calculator.py
+++ src/calculator.py
@@ -15,11 +15,18 @@ def multiply(x, y):
     return x * y
 
 def add_3_nums(x,y,z):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float)) and isinstance(z, (int, float))):
+        raise ValueError("Both inputs must be numbers.")
     total_sum = x + y + z
-    return total_sum
+    return x + y + z
 
 
-# f1_op = fun1(2,3)
-# f2_op = fun2(2,3)
-# f3_op = fun3(2,3)
-# f4_op = fun4(f1_op,f2_op,f3_op)
+# f1_op = add(2,3)
+# f2_op = substract(2,3)
+# f3_op = multiply(2,3)
+# f4_op = add_3_nums(f1_op,f2_op,f3_op)
+
+# print( f1_op)
+# print(f2_op)
+# print(f3_op)
+# print(f4_op)
